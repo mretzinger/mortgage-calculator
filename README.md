cbd90e0a81f0e4e739e6710c6cce1e5ec41c0e7c
Margaret Retzinger <maretzinger@gmail.com>
2021-06-15 09:06:03 -0400

    Learning Next.js Routing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mortgage-calculator
+
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/calculator.js
@@ -0,0 +1,10 @@
+export default function Calculator() {
+    return <h1>Hello World</h1>
+}
+//   export function MarCoolComponent(
+//       params = { 
+//           name: "World"
+//     }
+//   ) {
+//     return <h1>Hello {params.name}</h1>
+//   }
